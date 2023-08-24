4c0e18e757cd4a097bdb92662f20cc0f2066c418
Jochen Schmidt <jschmidt@cbs.mpg.de>
2023-08-24 16:57:33 +0200

    * define recon parameters

--- pypsi/options/recon_params.py
+++ pypsi/options/recon_params.py
@@ -48,32 +48,32 @@ class ReconParameters(sp.Serializable):
     ):
         if isinstance(te, np.ndarray):
             te = te.tolist()
-        self.multi_echo_img = ImageAcqParameters(
-            n_read=img_n_read, n_phase=img_n_phase, n_slice=img_n_slice,
-            resolution_read=img_resolution_read, resolution_phase=img_resolution_phase,
-            resolution_slice=img_resolution_slice, read_dir=read_dir,
-            acc_read=acc_read, acc_factor_phase=acc_factor_phase,
-            etl=etl, te=te, os_factor=os_factor
-        )
+        self.multi_echo_img.n_read = img_n_read
+        self.multi_echo_img.n_phase = img_n_phase
+        self.multi_echo_img.n_slice = img_n_slice
+        self.multi_echo_img.resolution_read = img_resolution_read
+        self.multi_echo_img.resolution_phase = img_resolution_phase
+        self.multi_echo_img.resolution_slice = img_resolution_slice
+        self.multi_echo_img.read_dir = read_dir
+        self.multi_echo_img.acc_read = acc_read
+        self.multi_echo_img.acc_factor_phase = acc_factor_phase
+        self.multi_echo_img.etl = etl
+        self.multi_echo_img.te = te
+        self.multi_echo_img.os_factor = os_factor
 
     def set_navigator_params(
             self, lines_per_nav: int, num_of_nav: int, nav_acc_factor: int, nav_resolution_scaling: float
     ):
-        nav_n_read = int(self.multi_echo_img.n_read * nav_resolution_scaling)
-        nav_n_phase = int(self.multi_echo_img.n_phase * nav_resolution_scaling)
-        nav_n_slice = int(self.multi_echo_img.n_slice * nav_resolution_scaling)
-        nav_res_read = self.multi_echo_img.n_read / nav_resolution_scaling
-        nav_res_phase = self.multi_echo_img.n_phase / nav_resolution_scaling
-        nav_res_slice = self.multi_echo_img.n_slice / nav_resolution_scaling
-        read_dir = self.multi_echo_img.read_dir
-        acc_read = False
-        acc_factor_phase = nav_acc_factor
-        self.navigator_img = NavigatorAcqParameters(
-            n_read=nav_n_read, n_phase=nav_n_phase, n_slice=nav_n_slice,
-            resolution_read=nav_res_read, resolution_phase=nav_res_phase,
-            resolution_slice=nav_res_slice, read_dir=read_dir,
-            acc_read=acc_read, acc_factor_phase=acc_factor_phase,
-            lines_per_nav=lines_per_nav, num_of_nav=num_of_nav, nav_acc_factor=nav_acc_factor,
-            nav_resolution_scaling=nav_resolution_scaling
-        )
-
+        self.navigator_img.n_read = int(self.multi_echo_img.n_read * nav_resolution_scaling)
+        self.navigator_img.n_phase = int(self.multi_echo_img.n_phase * nav_resolution_scaling)
+        self.navigator_img.n_slice = int(self.multi_echo_img.n_slice * nav_resolution_scaling)
+        self.navigator_img.resolution_read = self.multi_echo_img.n_read / nav_resolution_scaling
+        self.navigator_img.resolution_phase = self.multi_echo_img.n_phase / nav_resolution_scaling
+        self.navigator_img.resolution_slice = self.multi_echo_img.n_slice / nav_resolution_scaling
+        self.navigator_img.read_dir = self.multi_echo_img.read_dir
+        self.navigator_img.acc_read = False
+        self.navigator_img.acc_factor_phase = nav_acc_factor
+        self.navigator_img.lines_per_nav = lines_per_nav
+        self.navigator_img.num_of_nav = num_of_nav
+        self.navigator_img.nav_acc_factor = nav_acc_factor,
+        self.navigator_img.nav_resolution_scaling = nav_resolution_scaling
