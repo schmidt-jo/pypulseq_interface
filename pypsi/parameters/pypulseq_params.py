552c3f2619d7b541895e8b1704aed76f1b3ccd77
Jochen Schmidt <jschmidt@cbs.mpg.de>
2023-08-30 09:49:08 +0200

    * refactoring from camel case;

--- pypsi/parameters/pypulseq_params.py
+++ pypsi/parameters/pypulseq_params.py
@@ -17,110 +17,111 @@ class PypulseqParameters(sp.helpers.Serializable):
     report: bool = sp.field(default=False, alias="-r")
     visualize: bool = sp.field(default=True, alias="-v")
 
-    resolutionFovRead: float = 100  # [mm]
-    resolutionFovPhase: float = 100.0  # [%]
-    resolutionBase: int = 100
-    resolutionSliceThickness: float = 1.0  # [mm]
-    resolutionNumSlices: int = 10
-    resolutionSliceGap: int = 20  # %
+    resolution_fov_read: float = 100  # [mm]
+    resolution_fov_phase: float = 100.0  # [%]
+    resolution_base: int = 100
+    resolution_slice_thickness: float = 1.0  # [mm]
+    resolution_slice_num: int = 10
+    resolution_slice_gap: int = 20  # %
 
-    numberOfCentralLines: int = 40
-    accelerationFactor: float = 2.0
+    number_central_lines: int = 40
+    acceleration_factor: float = 2.0
 
-    excitationFA: float = 90.0
-    excitationRfPhase: float = 90.0  # °
-    excitationDuration: int = 2500  # [us]
-    excitationTimeBwProd: float = 2.0
-    excitationPreMoment: float = 1000.0  # Hz/m
-    excitationRephaseFactor: float = 1.04  # Correction factor for insufficient rephasing
+    excitation_rf_fa: float = 90.0
+    excitation_rf_phase: float = 90.0  # °
+    excitation_rf_time_bw_prod: float = 2.0
+    excitation_duration: int = 2500  # [us]
+    excitation_grad_moment_pre: float = 1000.0  # Hz/m
+    excitation_grad_rephase_factor: float = 1.04  # Correction factor for insufficient rephasing
 
-    refocusingFA: typing.List = dc.field(default_factory=lambda: [140.0])
-    refocusingRfPhase: typing.List = dc.field(default_factory=lambda: [0.0])  # °
-    refocusingDuration: int = 3000  # [us]
-    refocusingTimeBwProd: float = 2.0
-    refocusingScaleSliceGrad: float = 1.5  # adjust slice selective gradient sice of refocusing -
+    refocusing_rf_fa: typing.List = dc.field(default_factory=lambda: [140.0])
+    refocusing_rf_phase: typing.List = dc.field(default_factory=lambda: [0.0])  # °
+    refocusing_rf_time_bw_prod: float = 2.0
+    refocusing_duration: int = 3000  # [us]
+    refocusing_grad_slice_scale: float = 1.5  # adjust slice selective gradient sice of refocusing -
     # caution: this broadens the slice profile of the pulse, the further away from 180 fa
     # we possibly get saturation outside the slice
-    readSpoilingFactor: float = 0.5
-    sliceSpoilingMoment: float = 2500.0  # [Hz/m]
-    sliceEndSpoilingMoment: float = 2500  # [Hz/m]
-    interleavedAcquisition: bool = True
+    read_grad_spoiling_factor: float = 0.5
+    grad_moment_slice_spoiling: float = 2500.0  # [Hz/m]
+    grad_moment_slice_spoiling_end: float = 2500  # [Hz/m]
+    interleaved_acquisition: bool = True
     # interfacing with rfpf
-    extRfExc: str = ""
-    extRfRef: str = ""
+    ext_rf_exc: str = ""
+    ext_rf_ref: str = ""
 
-    ESP: float = 7.6  # [ms] echo spacing
-    ETL: int = 8  # echo train length
-    TR: float = 4500.0  # [ms]
+    esp: float = 7.6  # [ms] echo spacing
+    etl: int = 8  # echo train length
+    tr: float = 4500.0  # [ms]
 
     bandwidth: float = 250.0  # [Hz / px]
     oversampling: int = 2  # oversampling factor
-    sampleWeighting: float = 0.0  # factor to weight random sampling towards central k-space ->
+    sample_weighting: float = 0.0  # factor to weight random sampling towards central k-space ->
     # towards 1 we get densely sampled center
 
-    phaseDir: str = "PA"
+    phase_dir: str = "PA"
 
     def __post_init__(self):
         # resolution
-        self.resolutionNRead = self.resolutionBase  # number of freq encodes
-        self.resolutionNPhase = int(self.resolutionBase * self.resolutionFovPhase / 100)  # number of phase encodes
-        self.resolutionVoxelSizeRead = self.resolutionFovRead / self.resolutionBase  # [mm]
-        self.resolutionVoxelSizePhase = self.resolutionFovRead / self.resolutionBase  # [mm]
-        self.deltaK_read = 1e3 / self.resolutionFovRead  # cast to m
-        self.deltaK_phase = 1e3 / (self.resolutionFovRead * self.resolutionFovPhase / 100.0)  # cast to m
-        self.TE = np.arange(1, self.ETL + 1) * self.ESP  # [ms] echo times
+        self.resolution_n_read = self.resolution_base  # number of freq encodes
+        self.resolution_n_phase = int(self.resolution_base * self.resolution_fov_phase / 100)  # number of phase encodes
+        self.resolution_voxel_size_read = self.resolution_fov_read / self.resolution_base  # [mm]
+        self.resolution_voxel_size_phase = self.resolution_fov_read / self.resolution_base  # [mm]
+        self.delta_k_read = 1e3 / self.resolution_fov_read  # cast to m
+        self.delta_k_phase = 1e3 / (self.resolution_fov_read * self.resolution_fov_phase / 100.0)  # cast to m
+        self.te = np.arange(1, self.etl + 1) * self.esp  # [ms] echo times
         # there is one gap less than number of slices,
-        self.z_extend = self.resolutionSliceThickness * (
-                self.resolutionNumSlices + self.resolutionSliceGap / 100.0 * (self.resolutionNumSlices - 1))  # in mm
+        self.z_extend = self.resolution_slice_thickness * (
+                self.resolution_slice_num + self.resolution_slice_gap / 100.0 * (self.resolution_slice_num - 1)
+        )   # in mm
         # acc
-        self.numberOfOuterLines = round((self.resolutionNPhase - self.numberOfCentralLines) / self.accelerationFactor)
+        self.number_outer_lines = round((self.resolution_n_phase - self.number_central_lines) / self.acceleration_factor)
         # sequence
-        self.acquisitionTime = 1 / self.bandwidth
+        self.acquisition_time = 1 / self.bandwidth
         # dwell needs to be on adc raster time, acquisition time is flexible -> leads to small deviations in bandwidth
         # adc raster here hardcoded
         adc_raster = 1e-7
-        s_dwell = self.acquisitionTime / self.resolutionNRead / self.oversampling  # oversampling
+        s_dwell = self.acquisition_time / self.resolution_n_read / self.oversampling  # oversampling
         adcr_dwell = int(1 / adc_raster * s_dwell)
         self.dwell = adc_raster * adcr_dwell
         if np.abs(s_dwell - self.dwell > 1e-9):
             log_module.info(f"setting dwell time on adc raster -> small bw adoptions (set bw: {self.bandwidth:.1f})")
         # update acquisition time and bandwidth
-        self.acquisitionTime = self.dwell * self.resolutionNRead * self.oversampling
-        self.bandwidth = 1 / self.acquisitionTime
+        self.acquisition_time = self.dwell * self.resolution_n_read * self.oversampling
+        self.bandwidth = 1 / self.acquisition_time
         log_module.info(f"Bandwidth: {self.bandwidth:.1f} Hz/px;"
-                        f"Readout time: {self.acquisitionTime * 1e3:.1f} ms;"
+                        f"Readout time: {self.acquisition_time * 1e3:.1f} ms;"
                         f"DwellTime: {self.dwell * 1e6:.1f} us;"
-                        f"Number of Freq Encodes: {self.resolutionNRead}")
+                        f"Number of Freq Encodes: {self.resolution_n_read}")
         # ref list
-        if self.refocusingFA.__len__() != self.refocusingRfPhase.__len__():
-            err = f"provide same amount of refocusing pulse angle ({self.refocusingFA.__len__()}) " \
-                  f"and phases ({self.refocusingRfPhase.__len__()})"
+        if self.refocusing_rf_fa.__len__() != self.refocusing_rf_phase.__len__():
+            err = f"provide same amount of refocusing pulse angle ({self.refocusing_rf_fa.__len__()}) " \
+                  f"and phases ({self.refocusing_rf_phase.__len__()})"
             log_module.error(err)
             raise AttributeError(err)
         # check for phase values
-        for l_idx in range(self.refocusingRfPhase.__len__()):
-            while np.abs(self.refocusingRfPhase[l_idx]) > 180.0:
-                self.refocusingRfPhase[l_idx] = self.refocusingRfPhase[l_idx] - \
-                                                np.sign(self.refocusingRfPhase[l_idx]) * 180.0
-            while np.abs(self.refocusingFA[l_idx]) > 180.0:
-                self.refocusingFA[l_idx] = self.refocusingFA[l_idx] - np.sign(self.refocusingFA[l_idx]) * 180.0
-        while self.refocusingFA.__len__() < self.ETL:
+        for l_idx in range(self.refocusing_rf_phase.__len__()):
+            while np.abs(self.refocusing_rf_phase[l_idx]) > 180.0:
+                self.refocusing_rf_phase[l_idx] = self.refocusing_rf_phase[l_idx] - \
+                                                  np.sign(self.refocusing_rf_phase[l_idx]) * 180.0
+            while np.abs(self.refocusing_rf_fa[l_idx]) > 180.0:
+                self.refocusing_rf_fa[l_idx] = self.refocusing_rf_fa[l_idx] - np.sign(self.refocusing_rf_fa[l_idx]) * 180.0
+        while self.refocusing_rf_fa.__len__() < self.etl:
             # fill up list with last value
-            self.refocusingFA.append(self.refocusingFA[-1])
-            self.refocusingRfPhase.append(self.refocusingRfPhase[-1])
+            self.refocusing_rf_fa.append(self.refocusing_rf_fa[-1])
+            self.refocusing_rf_phase.append(self.refocusing_rf_phase[-1])
         # while self.sliceSpoilingMoment.__len__() < self.ETL:
         #     self.sliceSpoilingMoment.append(self.sliceSpoilingMoment[-1])
 
         # casting
-        self.excitationRadFA = self.excitationFA / 180.0 * np.pi
-        self.excitationRadRfPhase = self.excitationRfPhase / 180.0 * np.pi
-        self.refocusingRadFA = np.array(self.refocusingFA) / 180.0 * np.pi
-        self.refocusingRadRfPhase = np.array(self.refocusingRfPhase) / 180.0 * np.pi
+        self.excitation_rf_rad_fa = self.excitation_rf_fa / 180.0 * np.pi
+        self.excitation_rf_rad_phase = self.excitation_rf_phase / 180.0 * np.pi
+        self.refocusing_rf_rad_fa = np.array(self.refocusing_rf_fa) / 180.0 * np.pi
+        self.refocusing_rf_rad_phase = np.array(self.refocusing_rf_phase) / 180.0 * np.pi
         self.get_voxel_size()
-        if self.phaseDir == "PA":
+        if self.phase_dir == "PA":
             self.read_dir = 'x'
             self.phase_dir = 'y'
-        elif self.phaseDir == "RL":
+        elif self.phase_dir == "RL":
             self.phase_dir = 'x'
             self.read_dir = 'y'
         else:
@@ -129,7 +130,7 @@ class PypulseqParameters(sp.helpers.Serializable):
             raise AttributeError(err)
 
         # error catches
-        if np.any(np.array(self.sliceSpoilingMoment) < 1e-7):
+        if np.any(np.array(self.grad_moment_slice_spoiling) < 1e-7):
             err = f"this implementation needs a spoiling moment supplied: provide spoiling Moment > 0"
             log_module.error(err)
             raise ValueError(err)
@@ -137,12 +138,12 @@ class PypulseqParameters(sp.helpers.Serializable):
     def get_voxel_size(self):
         log_module.info(
             f"Voxel Size [read, phase, slice] in mm: "
-            f"{[self.resolutionVoxelSizeRead, self.resolutionVoxelSizePhase, self.resolutionSliceThickness]}")
-        return self.resolutionVoxelSizeRead, self.resolutionVoxelSizePhase, self.resolutionSliceThickness
+            f"{[self.resolution_voxel_size_read, self.resolution_voxel_size_phase, self.resolution_slice_thickness]}")
+        return self.resolution_voxel_size_read, self.resolution_voxel_size_phase, self.resolution_slice_thickness
 
     def get_fov(self):
-        fov_read = 1e-3 * self.resolutionFovRead
-        fov_phase = 1e-3 * self.resolutionFovRead * self.resolutionFovPhase / 100
+        fov_read = 1e-3 * self.resolution_fov_read
+        fov_phase = 1e-3 * self.resolution_fov_read * self.resolution_fov_phase / 100
         fov_slice = self.z_extend * 1e-3
         if self.read_dir == 'x':
             log_module.info(
